3929513e27c9bda0c81bc5035ea01457176b5f7a
Dany <daaldy4@gmail.com>
2025-02-16 08:21:02 -0600

    lectura inicial de archivos txt añadido

--- src/main/java/com/compi/Main.java
+++ src/main/java/com/compi/Main.java
@@ -1,7 +1,47 @@
 package com.compi;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));        
+        System.out.println("Por favor ingresar la ruta del archivo .txt que contiene su lenguaje. Solamente se aceptan archivos .txt\n");
+
+        String path = null;
+        try {
+            path = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        if (!path.endsWith(".txt")) {
+            throw new Exception("El archivo ingresado no es un archivo .txt");
+        }
+
+        String file = getFile(path);
+
+        System.out.println(file);
+
+    }
+
+    public static String getFile(String path){
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+            reader.close();
+            return stringBuilder.toString();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
     }
 }
