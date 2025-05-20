ab9ba74991965564a55a32ace0c6032eb729feed
Daniel Alberto Díaz Pérez <123769443+Dan0162@users.noreply.github.com>
2025-05-20 17:18:24 -0600

    Update Evaluador.java
    Se agregaron las funciones indent, getExportProg, getExportName, visitProg, visitHeader, visitSegvar, visitDefvar, visitSegfunct, y visitDeffunct.

--- src/main/java/com/compi/Evaluador.java
+++ src/main/java/com/compi/Evaluador.java
@@ -23,180 +23,92 @@ import com.compi.Gramatica_ANLTR4.ProjParser.SegvarContext;
 
 public class Evaluador extends ProjBaseVisitor<String>{
 
+ private String exportProg = "";
+    private String exportName = "";
+    private int indentLevel = 0;
+    
+    private String indent() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < indentLevel; i++) {
+            sb.append("    ");
+        }
+        return sb.toString();
+    }
+
+    public String getExportProg() {
+        return exportProg;
+    }
+    
+    public String getExportName() {
+        return exportName;
+    }
+
     @Override
-    public String visitBody_program(Body_programContext ctx) {
-        System.out.println("Visit: Body_program");
-        
-        exportProg += "\n" + indent() + "public static void main(String[] args) {\n";
-        indentLevel++;
-        
+    public String visitProg(ProgContext ctx) {
+        System.out.println("Visit: Prog");
         return visitChildren(ctx);
     }
 
     @Override
-    public String visitBodyfunct(BodyfunctContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: Bodyfunct");
-        return super.visitBodyfunct(ctx);
+    public String visitHeader(HeaderContext ctx) {
+        System.out.println("Visit: Header");
+        exportName = ctx.ID().getText();
+        exportProg = "public class " + exportName + " {\n";
+        return visitChildren(ctx);
     }
 
     @Override
-    public String visitBodysegE(BodysegEContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: BodysegE");
-        return super.visitBodysegE(ctx);
+    public String visitSegvar(SegvarContext ctx) {
+        System.out.println("Visit: Segvar");
+        return visitChildren(ctx);
     }
 
     @Override
-    public String visitBodysegE2(BodysegE2Context ctx) {
-        System.out.println("Visit: BodysegE2");
-        // TODO Auto-generated method stub
-        return super.visitBodysegE2(ctx);
-    }
-
-    @Override
-    public String visitBodysegT(BodysegTContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: BodysegT");
-        return super.visitBodysegT(ctx);
-    }
-
-    @Override
-    public String visitBodysegT2(BodysegT2Context ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: BodysegT2");
-        return super.visitBodysegT2(ctx);
-    }
-
-    @Override
-    public String visitCall_funct(Call_functContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: Call_funct");
-        return super.visitCall_funct(ctx);
-    }
-
-     @Override
-    public String visitDef_cond(Def_condContext ctx) {
-        System.out.println("Visit: Def_cond");
+    public String visitDefvar(DefvarContext ctx) {
+        System.out.println("Visit: Def_var");
         
-        exportProg += indent() + "if (";
-        visit(ctx.def_exp());
-        exportProg += ") {\n";
-        
-        indentLevel++;
-        visit(ctx.def_w());
-        indentLevel--;
-        exportProg += indent() + "}";
-        
-        if (ctx.else_cond() != null) {
-            exportProg += " ";
-            visit(ctx.else_cond());
-        } else {
-            exportProg += "\n";
-        }
-        
-        return null;
-    }
-
-
-     @Override
-    public String visitDef_exp(Def_expContext ctx) {
-        System.out.println("Visit: Def_exp");
-        
-        String id = ctx.ID().getText();
-        
-        if (ctx.COMP() != null) {
-            exportProg += id + " " + ctx.COMP().getText() + " " + ctx.Digitos().getText();
-        } else if (ctx.EQUAL() != null) {
-            if (ctx.Digitos() != null) {
-                exportProg += id + " " + convertEqualOperator(ctx.EQUAL().getText()) + " " + ctx.Digitos().getText();
-            } else {
-                exportProg += id + " " + convertEqualOperator(ctx.EQUAL().getText()) + " " + 
-                             (ctx.getText().contains("true") ? "true" : "false");
+        if (ctx.ID() != null) {
+            if ("int".equals(ctx.getChild(0).getText())) {
+                exportProg += indent() + "public static int " + ctx.ID().getText() + " = " + ctx.Digitos(0).getText() + ";\n";
+            } else if ("float".equals(ctx.getChild(0).getText())) {
+                exportProg += indent() + "public static float " + ctx.ID().getText() + " = " + 
+                              ctx.Digitos(0).getText() + "." + ctx.Digitos(1).getText() + "f;\n";
+            } else if ("string".equals(ctx.getChild(0).getText())) {
+                exportProg += indent() + "public static String " + ctx.ID().getText() + " = " + ctx.Cadena().getText() + ";\n";
+            } else if ("bool".equals(ctx.getChild(0).getText())) {
+                exportProg += indent() + "public static boolean " + ctx.ID().getText() + " = " + 
+                              (ctx.getText().contains("true") ? "true" : "false") + ";\n";
             }
         }
-        
         return null;
-    }
-    
-    private String convertEqualOperator(String op) {
-        if ("==".equals(op)) return "==";
-        if ("!=".equals(op)) return "!=";
-        return op; // Default fallback
     }
 
     @Override
-    public String visitDef_w(Def_wContext ctx) {
-        System.out.println("Visit: Def_w");
-        
-        if (ctx.getText().contains("output")) {
-            String content;
-            if (ctx.Cadena() != null) {
-                content = ctx.Cadena().getText();
-            } else if (ctx.ID() != null) {
-                content = ctx.ID().getText();
-            } else {
-                content = "\"\""; // Empty string as fallback
-            }
-            exportProg += indent() + "System.out.println(" + content + ");\n";
-        } else if (ctx.getText().contains("input")) {
-            String varName = ctx.ID().getText();
-            
-            if (!exportProg.contains("Scanner scanner")) {
-                exportProg += indent() + "java.util.Scanner scanner = new java.util.Scanner(System.in);\n";
-            }
-            
-            String varType = determineVariableType(varName);
-            String scannerMethod = getScannerMethodForType(varType);
-            
-            exportProg += indent() + varName + " = scanner." + scannerMethod + ";\n";
-        }
-        
-        return null;
-    }
-   
-
-   @Override
-    public String visitDef_while(Def_whileContext ctx) {
-        System.out.println("Visit: Def_while");
-        
-        String id = ctx.ID(0).getText();
-        String comp = ctx.COMP().getText();
-        String value = ctx.Digitos().getText();
-        
-        exportProg += indent() + "while (" + id + " " + comp + " " + value + ") {\n";
-        
-        indentLevel++;
-        visit(ctx.def_w());
-        
-        String updateId = ctx.ID(1).getText();
-        String op = ctx.getChild(8).getText();
-        if ("+".equals(op)) {
-            exportProg += indent() + updateId + "++;\n";
-        } else if ("-".equals(op)) {
-            exportProg += indent() + updateId + "--;\n";
-        }
-        
-        indentLevel--;
-        exportProg += indent() + "}\n";
-        
-        return null;
-    }
-
-
-    @Override
-    public String visitDefarith(DefarithContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: Def_arith");
-        return super.visitDefarith(ctx);
+    public String visitSegfunct(SegfunctContext ctx) {
+        System.out.println("Visit: Segfunct");
+        return visitChildren(ctx);
     }
 
     @Override
     public String visitDeffunct(DeffunctContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: Def_funct");
-        return super.visitDeffunct(ctx);
+        System.out.println("Visit: Deffunct");
+        if (ctx.ID().size() >= 2) {
+            String functionName = ctx.ID(0).getText();
+            String paramName = ctx.ID(1).getText();
+            
+            exportProg += indent() + "public static int " + functionName + "(" + "int " + paramName + ") {\n";
+            indentLevel++;
+            visit(ctx.bodyfunct());
+            indentLevel--;
+            exportProg += indent() + "}\n\n";
+            
+            if (ctx.deffunct() != null) {
+                visit(ctx.deffunct());
+            }
+        }
+        return null;
     }
+
 
     @Override
     public String visitDefvar(DefvarContext ctx) {
