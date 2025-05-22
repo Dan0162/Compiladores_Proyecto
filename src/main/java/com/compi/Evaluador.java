a5c6ae824c471cbf308f398b0db4d7a20f47136f
Daniel Alberto Díaz Pérez <123769443+Dan0162@users.noreply.github.com>
2025-05-21 18:30:18 -0600

    Update Evaluador.java

--- src/main/java/com/compi/Evaluador.java
+++ src/main/java/com/compi/Evaluador.java
@@ -1,29 +1,11 @@
 package com.compi;
 
 import com.compi.Gramatica_ANLTR4.ProjBaseVisitor;
-import com.compi.Gramatica_ANLTR4.ProjParser.Body_programContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.BodyfunctContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.BodysegE2Context;
-import com.compi.Gramatica_ANLTR4.ProjParser.BodysegEContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.BodysegT2Context;
-import com.compi.Gramatica_ANLTR4.ProjParser.BodysegTContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.Call_functContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.Def_condContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.Def_expContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.Def_wContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.Def_whileContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.DefarithContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.DeffunctContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.DefvarContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.Else_condContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.ItemarithContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.ProgContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.SegfunctContext;
-import com.compi.Gramatica_ANLTR4.ProjParser.SegvarContext;
+import com.compi.Gramatica_ANLTR4.ProjParser.*;
 
-public class Evaluador extends ProjBaseVisitor<String>{
+public class Evaluador extends ProjBaseVisitor<String> {
 
- private String exportProg = "";
+    private String exportProg = "";
     private String exportName = "";
     private int indentLevel = 0;
     
@@ -109,15 +91,160 @@ public class Evaluador extends ProjBaseVisitor<String>{
         return null;
     }
 
+    @Override
+    public String visitBodyfunct(BodyfunctContext ctx) {
+        System.out.println("Visit: Bodyfunct");
+        
+        if (ctx.defarith() != null) {
+            StringBuilder expressionBuilder = new StringBuilder();
+            processDefarith(ctx.defarith(), expressionBuilder);
+            
+            if (!expressionBuilder.toString().isEmpty()) {
+                if (ctx.defarith().bodysegE() != null) {
+                    String paramName = ctx.ID().getText();
+                    exportProg += indent() + paramName + " = " + expressionBuilder.toString() + ";\n";
+                }
+            }
+        }
+        
+        if (ctx.ID() != null) {
+            exportProg += indent() + "return " + ctx.ID().getText() + ";\n";
+        }
+        
+        return null;
+    }
+    
+    private void processDefarith(DefarithContext ctx, StringBuilder builder) {
+        if (ctx.bodysegE() != null) {
+            processExpression(ctx.bodysegE(), builder);
+        }
+    }
 
     @Override
-    public String visitDefvar(DefvarContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: Def_var");
-        if (ctx.ID().equals("int")){
+    public String visitDefarith(DefarithContext ctx) {
+        System.out.println("Visit: Defarith");
+        return visitChildren(ctx);
+    }
 
+    @Override
+    public String visitBodysegE(BodysegEContext ctx) {
+        StringBuilder expressionBuilder = new StringBuilder();
+        processExpression(ctx, expressionBuilder);
+        
+        return expressionBuilder.toString();
+    }
+
+    private void processExpression(BodysegEContext ctx, StringBuilder builder) {
+        processBodysegT(ctx.bodysegT(), builder);
+        
+        if (ctx.bodysegE2() != null) {
+            processBodysegE2(ctx.bodysegE2(), builder);
         }
-        return super.visitDefvar(ctx);
+    }
+
+    private void processBodysegE2(BodysegE2Context ctx, StringBuilder builder) {
+        if (ctx.getChildCount() > 0) {
+            builder.append(" ").append(ctx.getChild(0).getText()).append(" ");
+            
+            processBodysegT(ctx.bodysegT(), builder);
+            
+            if (ctx.bodysegE2() != null) {
+                processBodysegE2(ctx.bodysegE2(), builder);
+            }
+        }
+    }
+
+    private void processBodysegT(BodysegTContext ctx, StringBuilder builder) {
+        processItemArith(ctx.itemarith(), builder);
+        
+        if (ctx.bodysegT2() != null) {
+            processBodysegT2(ctx.bodysegT2(), builder);
+        }
+    }
+
+    private void processBodysegT2(BodysegT2Context ctx, StringBuilder builder) {
+        if (ctx.getChildCount() > 0) {
+            builder.append(" ").append(ctx.getChild(0).getText()).append(" ");
+            
+            processItemArith(ctx.itemarith(), builder);
+            
+            if (ctx.bodysegT2() != null) {
+                processBodysegT2(ctx.bodysegT2(), builder);
+            }
+        }
+    }
+
+    private void processItemArith(ItemarithContext ctx, StringBuilder builder) {
+        if (ctx.ID() != null) {
+            builder.append(ctx.ID().getText());
+        } else if (ctx.Digitos() != null) {
+            builder.append(ctx.Digitos().getText());
+        } else if (ctx.bodysegE() != null) {
+            builder.append("(");
+            StringBuilder nestedBuilder = new StringBuilder();
+            processExpression(ctx.bodysegE(), nestedBuilder);
+            builder.append(nestedBuilder);
+            builder.append(")");
+        }
+    }
+
+    @Override
+    public String visitBody_program(Body_programContext ctx) {
+        System.out.println("Visit: Body_program");
+        
+        exportProg += "\n" + indent() + "public static void main(String[] args) {\n";
+        indentLevel++;
+        
+        return visitChildren(ctx);
+    }
+
+    @Override
+    public String visitDef_cond(Def_condContext ctx) {
+        System.out.println("Visit: Def_cond");
+        
+        exportProg += indent() + "if (";
+        visit(ctx.def_exp());
+        exportProg += ") {\n";
+        
+        indentLevel++;
+        visit(ctx.def_w());
+        indentLevel--;
+        exportProg += indent() + "}";
+        
+        if (ctx.else_cond() != null) {
+            exportProg += " ";
+            visit(ctx.else_cond());
+        } else {
+            exportProg += "\n";
+        }
+        
+        return null;
+    }
+
+    @Override
+    public String visitDef_exp(Def_expContext ctx) {
+        System.out.println("Visit: Def_exp");
+        
+        String id = ctx.ID().getText();
+        
+        if (ctx.COMP() != null) {
+            exportProg += id + " " + ctx.COMP().getText() + " " + ctx.Digitos().getText();
+        } else if (ctx.EQUAL() != null) {
+            if (ctx.Digitos() != null) {
+                exportProg += id + " " + convertEqualOperator(ctx.EQUAL().getText()) + " " + ctx.Digitos().getText();
+            } else {
+                exportProg += id + " " + convertEqualOperator(ctx.EQUAL().getText()) + " " + 
+                             (ctx.getText().contains("true") ? "true" : "false");
+            }
+        }
+        
+        return null;
+    }
+    
+    private String convertEqualOperator(String op) {
+        if ("==".equals(op)) return "==";
+        if ("!=".equals(op)) return "!=";
+        return op; 
     }
 
     @Override
@@ -135,34 +262,66 @@ public class Evaluador extends ProjBaseVisitor<String>{
         return null;
     }
 
-
     @Override
-    public String visitItemarith(ItemarithContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: Item_arith");
-        return super.visitItemarith(ctx);
+    public String visitDef_while(Def_whileContext ctx) {
+        System.out.println("Visit: Def_while");
+        
+        String id = ctx.ID(0).getText();
+        String comp = ctx.COMP().getText();
+        String value = ctx.Digitos().getText();
+        
+        exportProg += indent() + "while (" + id + " " + comp + " " + value + ") {\n";
+        
+        indentLevel++;
+        visit(ctx.def_w());
+        
+        String updateId = ctx.ID(1).getText();
+        String op = ctx.getChild(8).getText();
+        if ("+".equals(op)) {
+            exportProg += indent() + updateId + "++;\n";
+        } else if ("-".equals(op)) {
+            exportProg += indent() + updateId + "--;\n";
+        }
+        
+        indentLevel--;
+        exportProg += indent() + "}\n";
+        
+        return null;
     }
 
     @Override
-    public String visitProg(ProgContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: Prog");
-        return super.visitProg(ctx);
+    public String visitDef_w(Def_wContext ctx) {
+        System.out.println("Visit: Def_w");
+        
+        if (ctx.getText().contains("output")) {
+            String content;
+            if (ctx.Cadena() != null) {
+                content = ctx.Cadena().getText();
+            } else if (ctx.ID() != null) {
+                content = ctx.ID().getText();
+            } else {
+                content = "\"\""; 
+            }
+            exportProg += indent() + "System.out.println(" + content + ");\n";
+        } else if (ctx.getText().contains("input")) {
+            String varName = ctx.ID().getText();
+            
+            if (!exportProg.contains("Scanner scanner")) {
+                exportProg += indent() + "java.util.Scanner scanner = new java.util.Scanner(System.in);\n";
+            }
+            
+            String varType = determineVariableType(varName);
+            String scannerMethod = getScannerMethodForType(varType);
+            
+            exportProg += indent() + varName + " = scanner." + scannerMethod + ";\n";
+        }
+        
+        return null;
     }
+    
 
-    @Override
-    public String visitSegfunct(SegfunctContext ctx) {
-        // TODO Auto-generated method stub
-        System.out.println("Visit: Segfunct"); //inicia evaluacion
-        return super.visitSegfunct(ctx);
-    }
-
-    @Override
-    public String visitSegvar(SegvarContext ctx) {
-        System.out.println("Visit: Segvar");       
-        return super.visitSegvar(ctx);
-    }
- private String determineVariableType(String varName) {
+    private String determineVariableType(String varName) {
+        // Check for types in the generated code
         if (exportProg.contains("int " + varName + " =")) {
             return "int";
         } else if (exportProg.contains("float " + varName + " =")) {
@@ -173,6 +332,7 @@ public class Evaluador extends ProjBaseVisitor<String>{
             return "boolean";
         }
         
+        // Default to int if type can't be determined
         return "int";
     }
     
@@ -214,106 +374,8 @@ public class Evaluador extends ProjBaseVisitor<String>{
         indentLevel--;
         exportProg += indent() + "}\n";
         
-        exportProg += "}\n";
+       exportProg += "}\n";
         
         return null;
     }
-
- // Desde acá - JL20250521
-   @Override
-     public String visitBodyfunct(BodyfunctContext ctx) {
-         System.out.println("Visit: Bodyfunct");
-         
-         if (ctx.defarith() != null) {
-             StringBuilder expressionBuilder = new StringBuilder();
-             processDefarith(ctx.defarith(), expressionBuilder);
-             
-             if (!expressionBuilder.toString().isEmpty()) {
-                 if (ctx.defarith().bodysegE() != null) {
-                     String paramName = ctx.ID().getText();
-                     exportProg += indent() + paramName + " = " + expressionBuilder.toString() + ";\n";
-                 }
-             }
-         }
-         
-         if (ctx.ID() != null) {
-             exportProg += indent() + "return " + ctx.ID().getText() + ";\n";
-         }
-         
-         return null;
-     }
-     
-     private void processDefarith(DefarithContext ctx, StringBuilder builder) {
-         if (ctx.bodysegE() != null) {
-             processExpression(ctx.bodysegE(), builder);
-         }
-     }
- 
-     @Override
-     public String visitDefarith(DefarithContext ctx) {
-         System.out.println("Visit: Defarith");
-         return visitChildren(ctx);
-     }
- 
-     @Override
-     public String visitBodysegE(BodysegEContext ctx) {
-         StringBuilder expressionBuilder = new StringBuilder();
-         processExpression(ctx, expressionBuilder);
-         
-         return expressionBuilder.toString();
-     }
- 
-     private void processExpression(BodysegEContext ctx, StringBuilder builder) {
-         processBodysegT(ctx.bodysegT(), builder);
-         
-         if (ctx.bodysegE2() != null) {
-             processBodysegE2(ctx.bodysegE2(), builder);
-         }
-     }
- 
-     private void processBodysegE2(BodysegE2Context ctx, StringBuilder builder) {
-         if (ctx.getChildCount() > 0) {
-             builder.append(" ").append(ctx.getChild(0).getText()).append(" ");
-             
-             processBodysegT(ctx.bodysegT(), builder);
-             
-             if (ctx.bodysegE2() != null) {
-                 processBodysegE2(ctx.bodysegE2(), builder);
-             }
-         }
-     }
- 
-     private void processBodysegT(BodysegTContext ctx, StringBuilder builder) {
-         processItemArith(ctx.itemarith(), builder);
-         
-         if (ctx.bodysegT2() != null) {
-             processBodysegT2(ctx.bodysegT2(), builder);
-         }
-     }
- 
-     private void processBodysegT2(BodysegT2Context ctx, StringBuilder builder) {
-         if (ctx.getChildCount() > 0) {
-             builder.append(" ").append(ctx.getChild(0).getText()).append(" ");
-             
-             processItemArith(ctx.itemarith(), builder);
-             
-             if (ctx.bodysegT2() != null) {
-                 processBodysegT2(ctx.bodysegT2(), builder);
-             }
-         }
-     }
- 
-     private void processItemArith(ItemarithContext ctx, StringBuilder builder) {
-         if (ctx.ID() != null) {
-             builder.append(ctx.ID().getText());
-         } else if (ctx.Digitos() != null) {
-             builder.append(ctx.Digitos().getText());
-         } else if (ctx.bodysegE() != null) {
-             builder.append("(");
-             StringBuilder nestedBuilder = new StringBuilder();
-             processExpression(ctx.bodysegE(), nestedBuilder);
-             builder.append(nestedBuilder);
-             builder.append(")");
-         }
-     }
 }
